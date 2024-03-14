4b0d20ef31280619a0d9a00bb4408ad67aa0eb6c
Jocelyn Berenice Zamora De La Cerda <201G0293@rcarbonifera.tecnm.mx>
2024-03-14 15:40:39 -0600

    comentarios

--- AppInventario/Data/ArticuloDBContext.cs
+++ AppInventario/Data/ArticuloDBContext.cs
@@ -49,6 +49,7 @@ namespace AppInventario.Data
             }
         }
         //CRUD
+        //optiene solo un resultado sacado por medio del id y se utiliza reader para obtener el valor en especifico de la tabla
         public async Task<Articulos?> GetById(int id)
         {
             Articulos? articulo = null;
@@ -68,6 +69,7 @@ namespace AppInventario.Data
                     await reader.ReadAsync();
                     articulo = new Articulos
                     {
+                        //todos tienen un tipo de get donde se especifica tipo de variable dependiendo de la variable que sea, y los nums es el puesto de columna que optienen
                         id = reader.GetInt32(0),
                         descripcion = reader.GetString(1),
                         precio = reader.GetDecimal(2),
@@ -79,6 +81,7 @@ namespace AppInventario.Data
             }
             return articulo;
         }
+        //obtiene todos los resultados
         public async Task<IEnumerable<Articulos>> GetAll()
         {
             List<Articulos> ?listarticulo = null;
@@ -106,6 +109,7 @@ namespace AppInventario.Data
                 return listarticulo;
             }
         }
+        //Falta revisarlos en clase pero parece que estan bien escritos, ya que las acciones se hacen con medio de querys 
         public async Task Eliminar(int id) 
         {
             using (var connection = new SqliteConnection(_ConectionString)) 
@@ -114,7 +118,7 @@ namespace AppInventario.Data
                 var command = connection.CreateCommand();
                 command.CommandText = (@"DELTE FROM articulo WHERE id = $id");
                 command.Parameters.AddWithValue("$id", id);
-                //command.Parameters.Remove("id");
+                //command.Parameters.Remove("id"); //este creo que esta de mas la vdd ignorar
                 await command.ExecuteScalarAsync();
             }
         }
